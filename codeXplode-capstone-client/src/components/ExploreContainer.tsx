6f0b858b31d6c0506dda151c70774e78128de402
RobbieMac82 <robvmacdonald@gmail.com>
2023-06-21 01:21:06 -0400

    Initial Successful ionic launch no render yet

--- codeXplode-capstone-client/src/components/ExploreContainer.tsx
+++ codeXplode-capstone-client/src/components/ExploreContainer.tsx
@@ -1,14 +1,25 @@
 import './ExploreContainer.css';
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<h1>Hello.... Hello...</h1>
+			{/* 
+			<strong>Ready to create an app?</strong>
+			<p>
+				Start with Ionic{' '}
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href='https://ionicframework.com/docs/components'
+				>
+					UI Components
+				</a>
+			</p> */}
+		</div>
+	);
 };
 
 export default ExploreContainer;
--- codeXplode-capstone-client/src/pages/Home.tsx
+++ codeXplode-capstone-client/src/pages/Home.tsx
@@ -1,25 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Hi</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse='condense'>
+					<IonToolbar>
+						<IonTitle size='large'>Blank</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<h1>Hi</h1>
+				<ExploreContainer />
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
