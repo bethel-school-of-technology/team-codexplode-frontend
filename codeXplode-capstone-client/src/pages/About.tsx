300f67fffaf6d38293701e76c99d354ab8fdcc81
unknown <beguerascatherine8@gmail.com>
2023-07-06 08:54:47 +0800

    Cath-about page

--- codeXplode-capstone-client/src/pages/About.tsx
+++ codeXplode-capstone-client/src/pages/About.tsx
@@ -5,27 +5,30 @@ import {
 	IonMenuButton,
 	IonPage,
 	IonTitle,
-	IonToolbar
+	IonToolbar,
+	IonCard,
 } from '@ionic/react';
-import BlankPage from '../components/BlankPage';
 import './About.css';
 
 const About: React.FC = () => {
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonButtons slot='start'>
-						<IonMenuButton></IonMenuButton>
-					</IonButtons>
-					<IonTitle>About</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent className='ion-padding'>
-				<BlankPage />
-			</IonContent>
-		</IonPage>
-	);
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>About</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+            <IonCard>
+                <img alt="" src= "C:\Users\bethe\Team_CodeXplode_Project\team-codexplode-frontend\codeXplode-capstone-client\resources\icon.png" />
+            </IonCard>
+                <div id="container">
+                <h1>Hey There Foodies!</h1>
+                <p>"A social platform to build connections between people through the wonderful medium of foods"</p>
+                <p>Food is a great connector, Come!, Let's build beautiful community through foods!</p>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
-
 export default About;
