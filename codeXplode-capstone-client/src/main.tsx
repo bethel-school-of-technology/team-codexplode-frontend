33c77d24751b71eec81d951a62d065341b6662bd
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-07-18 21:35:26 -0700

    logining in user works and stores a JWT, given that a user is generated beforehand using Postman

--- codeXplode-capstone-client/src/main.tsx
+++ codeXplode-capstone-client/src/main.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import { EventProvider } from "./contexts/EventContext";
+import { EventProvider } from './contexts/EventContext';
+import { UserProvider } from './contexts/UserContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-	<React.StrictMode>
-		<EventProvider>
-			<App />
-		</EventProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <UserProvider>
+      <EventProvider>
+        <App />
+      </EventProvider>
+    </UserProvider>
+  </React.StrictMode>
 );
 // Calling the element loader after the app has been rendered the first time
 defineCustomElements(window);
